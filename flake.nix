4975ff506dad806ae50250ca6e1d55bc711d1715
Kirill Kuznetsov <kdevlab@yandex.ru>
2025-05-05 13:37:23 +0300

    Simplify build

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     # the nixpkgs version shipped with the nix-portable executable
     # TODO: find out why updating this leads to error when building pkgs.hello:
     # Error: checking whether build environment is sane... ls: cannot access './configure': No such file or directory
-    defaultChannel.url = "nixpkgs/nixos-24.05";
+    defaultChannel.url = "nixpkgs/nixos-unstable";
 
     nix.url = "nix/2.25.2";
   };
@@ -131,27 +131,21 @@
 
           # the static proot built with nix somehow didn't work on other systems,
           # therefore using the proot static build from proot gitlab
-          proot = if crossSystem != null then throw "fix proot for crossSytem" else import ./proot/github.nix { inherit pkgs; };
+          prootStatic = if crossSystem != null then throw "fix proot for crossSytem" else import ./proot/alpine.nix { inherit pkgs; };
         in
           # crashes if nixpkgs updated: error: executing 'git': No such file or directory
           pkgs.callPackage ./default.nix {
 
-            inherit proot;
+            #proot = prootStatic;
 
             pkgs = pkgsDefaultChannel;
 
             lib = inp.nixpkgs.lib;
-            compression = "zstd -3 -T1";
 
             nix = inp.nix.packages.${system}.nix;
             nixStatic = inp.nix.packages.${system}.nix-static;
 
-            busybox = pkgs.pkgsStatic.busybox;
-            bwrap = pkgs.pkgsStatic.bubblewrap;
-            gnutar = pkgs.pkgsStatic.gnutar;
-            perl = pkgs.pkgsBuildBuild.perl;
-            xz = pkgs.pkgsStatic.xz;
-            zstd = pkgs.pkgsStatic.zstd;
+            pkgsStatic = pkgs.pkgsStatic;
 
             # tar crashed on emulated aarch64 system
             buildSystem = "x86_64-linux";
