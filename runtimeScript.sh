ae1bc2b559b8715fee60d70cf5d49269fe825644
Kirill Kuznetsov <kdevlab@yandex.ru>
2025-05-11 16:44:35 +0300

    Disable overlays in NIX_PATH

--- runtimeScript.sh
+++ runtimeScript.sh
@@ -433,7 +433,9 @@ debug "base command will be: $run"
 
 
 ### setup environment
-export NIX_PATH="$dir/channels:nixpkgs=$dir/channels/nixpkgs"
+emptyPath="$dir/nixpkgs-empty"
+export NIX_PATH="$dir/channels:nixpkgs=$dir/channels/nixpkgs:nixpkgs-overlays=$emptyPath"
+mkdir -p "$emptyPath"
 mkdir -p "$dir"/channels
 [ -h "$dir"/channels/nixpkgs ] || ln -s $nixpkgsSrc "$dir"/channels/nixpkgs
 
