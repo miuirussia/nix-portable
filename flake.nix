610f66c09c52f12f2cd22d08cd054bad53d2aa8e
Kirill Kuznetsov <kdevlab@yandex.ru>
2025-05-20 14:47:40 +0300

    Update nix

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     defaultChannel.url = "github:miuirussia/nixpkgs/nixpkgs-unstable";
 
     # See latest done job https://hydra.nixos.org/job/nix/master/buildStatic.nix-everything.x86_64-linux/latest
-    nix.url = "github:NixOS/nix/469a6371ec44c2717f2508dcdec8a88a3a4b9f5b?narHash=sha256-uIamZku0xS58vB2isRgK5v9YTq%2B9IrJpPl9MNwQjLzg%3D";
+    nix.url = "github:NixOS/nix/8c10104e9ef5d3a86e5f3f56136a1b29b385a8dc?narHash=sha256-8cJeFOxPaSbuNdQtHZ7SAGkMFb9TD4pEub7z1BqrNyE%3D";
   };
 
   outputs =
