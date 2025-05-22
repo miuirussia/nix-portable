32232b693cb526a47a84cba41a9bd05021fa0558
Kirill Kuznetsov <kdevlab@yandex.ru>
2025-05-22 18:29:15 +0300

    Add workflow

--- update.py
+++ update.py
@@ -1,5 +1,19 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i python3 -p python3 python3Packages.requests
+
 import requests
 import json
+import re
+
+def replace_nix_url_in_file(file_path, new_url):
+    pattern = r'(\s+)nix\.url\s*=\s*"([^"]+)"\s*;'
+    with open(file_path, 'r') as file:
+        content = file.read()
+    
+    modified_content = re.sub(pattern, lambda match: f'{match.group(1)}nix.url = "{new_url}";', content)
+
+    with open(file_path, 'w') as file:
+        file.write(modified_content)
 
 # Step 1: Get the ID of the latest jobset evaluation
 url_id = 'https://hydra.nixos.org/job/nix/master/buildStatic.nix-everything.x86_64-linux/latest'
@@ -22,6 +36,4 @@ for eval_entry in data_flake['evals']:
         flake_result = eval_entry['flake']
         break
 
-# Print the flake
-print(flake_result)
-
+replace_nix_url_in_file('./flake.nix', flake_result)
